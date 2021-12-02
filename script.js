2cbd5fa24664ff6b7bf98465d1935a2816fdce94
moayad-tarig <software.moayad@gmail.com>
2021-12-02 02:03:14 -0800

    practice on vanillaa js

---
+++ script.js
@@ -0,0 +1,33 @@
+const ratings = document.querySelectorAll('.rating');
+const ratingContainer = document.querySelector('.rating-container');
+const sendBtn = document.querySelector('#send');
+const panel = document.querySelector('#panel');
+let selectedRating = 'Satisfied';
+
+ratingContainer.addEventListener('click', (e)=> {
+    if(e.target.parentNode.classList.contains('rating')){
+        removeActive()
+        e.target.parentNode.classList.add('active')
+        selectedRating = e.target.innerHTML;
+        console.log(e.target.innerHTML)
+        
+        
+    }
+   
+})
+
+sendBtn.addEventListener('click', (e)=>{
+    panel.innerHTML = `
+    <i class="fas fa-heart"></i>
+    <strong>Thank You</strong>
+    </br>
+    <strong>Feedback: ${selectedRating} </strong>
+    <p>We'll use your feedback to imporove our customer support</p>
+    `
+})
+
+function removeActive(){
+    for(let i = 0; i < ratings.length; i++){
+        ratings[i].classList.remove('active')
+    }
+}
